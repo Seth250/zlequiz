480aa2dc1115d275eb4a6c8533b02dcd9811e9c4
Seth250 <ayowaleakintayo@gmail.com>
2020-08-09 22:14:49 +0100

    initial-commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+		<Header />
+		<h2>App</h2>
+		<Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+		Header,
+		Footer
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
